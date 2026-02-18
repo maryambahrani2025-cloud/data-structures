05db7e5316d9b9e9721689afa37cf6396785b8e7
maryambahrani2025-cloud <maryambahrani2025@gmail.com>
2026-02-18 14:58:32 +0330

    Update python53.py

--- python53.py
+++ python53.py
@@ -1,11 +1,11 @@
 def_Del_last(self):
-if self. head is None:
+  if self. head is None:
     print('error0')
     Return
-c=self.head
-while c.next
-c=c.next
-if c.back is None:
+   c=self.head
+  while c.next
+    c=c.next
+  if c.back is None:
     self.Del_first()
     Return 
     c.back.next=None
