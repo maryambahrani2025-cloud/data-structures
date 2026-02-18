cc4a567b48dbba09dd6ad664538f36517b8e0a99
maryambahrani2025-cloud <maryambahrani2025@gmail.com>
2026-02-18 15:52:20 +0330

    Delete PY (9).py

--- PY (9).py
+++
@@ -1,7 +0,0 @@
-def count_2Deg(root):
-    if root is None:
-        return0
-        if #فرزند فقط راست بود
-        return count_2Deg(root.Lchild)
-    return count_2Deg(root.Lchild)
-+count_2Deg(root.Rchild)+1
